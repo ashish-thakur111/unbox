40e99d32d4571b54101a3286016f55c0351fc8d3
Ashish Thakur <59604322+ashish-thakur111@users.noreply.github.com>
2022-03-16 00:52:21 +0530

    initial commit

---
+++ pkg/models/Config.go
@@ -0,0 +1,13 @@
+package models
+
+type Config struct {
+	Name    string `yaml:"name"`
+	Base    string `yaml:"base"`
+	Repo    string `yaml:"repo"`
+	Context struct {
+		ServiceName string   `yaml:"serviceName"`
+		ProductName string   `yaml:"productName"`
+		Port        int      `yaml:"port"`
+		Volumes     []string `yaml:"volumes"`
+	}
+}
